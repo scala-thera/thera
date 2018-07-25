c8111e8654f867dc2881a46b91d72d08c7f7f51a
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2018-07-26 00:34:55 +0300

    Code to process all the blog posts

--- src/main/scala/thera/Main.scala
+++ src/main/scala/thera/Main.scala
@@ -3,6 +3,8 @@ package thera
 import scala.collection.JavaConverters._
 
 import java.io.File
+import java.util.Date
+import java.text.SimpleDateFormat
 import org.apache.commons.io.FileUtils
 
 import cats._, cats.implicits._
@@ -11,40 +13,73 @@ import io.circe._
 
 
 object Main {
-  def main(args: Array[String]): Unit = {
-    val input  = new File("site-src/posts/2017-03-10-matryoshka-intro.md")
-    val output = new File("_site/result.html")
-    val data   = new File("site-src/data/data.yml")
-    val assets = new File("site-src/assets")
+  val postsIn  = new File("site-src/posts/")
+  val postsOut = new File("_site/posts/")
 
-    run { for {
-      // Config
-      configRaw <- att { FileUtils.readFileToString(data, settings.enc) }
-      config    <- exn { yaml.parser.parse(configRaw) }
-      _         <- att { println(s"Config parsed:\n${config}") }
+  val data   = new File("site-src/data/data.yml")
+  val assets = new File("site-src/assets")
 
-      // Assemble assets
-      _   <- att { FileUtils.copyDirectory(assets, new File("_site", "assets")) }
-      css <- templates(
-        new File("site-src/private-assets/css/all.css")
-      , fragmentResolver = name => new File(s"site-src/private-assets/css/${name}.css"))
-      _   <- att { FileUtils.writeStringToFile(new File("_site/assets/all.css"), css, settings.enc) }
+  def main(args: Array[String]): Unit = run { for {
+    // Config
+    configRaw <- att { FileUtils.readFileToString(data, settings.enc) }
+    config    <- exn { yaml.parser.parse(configRaw) }
+    _         <- att { println(s"Config parsed:\n${config}") }
 
-      // Copy code directory
-      _ <- att { FileUtils.copyDirectory(new File("site-src/code"), new File("_site/code")) }
+    // Assemble assets
+    _   <- att { FileUtils.copyDirectory(assets, new File("_site", "assets")) }
+    css <- templates(
+      new File("site-src/private-assets/css/all.css")
+    , fragmentResolver = name => new File(s"site-src/private-assets/css/${name}.css"))
+    _   <- att { FileUtils.writeStringToFile(new File("_site/assets/all.css"), css, settings.enc) }
 
-      // Process input post
-      res <- templates(input, config)
-      _   <- att { FileUtils.writeStringToFile(output, res, settings.enc) }
+    // Copy code directory
+    _ <- att { FileUtils.copyDirectory(new File("site-src/code"), new File("_site/code")) }
 
-      // Delete the code directory
-      _ <- att { FileUtils.deleteDirectory(new File("_site/code")) }
-      // _ <- index(vars)
-    } yield () }
+    // Process input posts
+    allPosts <- att { readPosts(postsIn) }
+    _ <- allPosts.traverse(processPost(_, config))
+
+
+    // Delete the code directory
+    _ <- att { FileUtils.deleteDirectory(new File("_site/code")) }
+    // _ <- index(vars)
+  } yield () }
+
+  def readPosts(input: File): List[Post] =
+    FileUtils.iterateFiles(input, Array("md"), false)
+      .asScala.map(Post.fromFile).toList
+
+  def processPost(post: Post, globalConfig: Json): Ef[Unit] =
+    for {
+      config <- att { globalConfig.deepMerge(post.asJson) }
+      res    <- templates(post.inFile, config)
+      _      <- att { FileUtils.writeStringToFile(new File(postsOut, post.htmlName), res, settings.enc) }
+    } yield ()
+}
+
+case class Post(inFile: File, date: Date, title: String) {
+  lazy val htmlName: String =
+    inFile.getName.reverse.drop(2).reverse + "html"
+
+  lazy val url: String = s"/posts/$htmlName"
+  
+  lazy val dateStr: String = Post.dateFormatter.format(date)
+
+  lazy val asJson: Json = Json.obj(
+    "date"  -> Json.fromString(dateStr)
+  , "url"   -> Json.fromString(url    )
+  , "title" -> Json.fromString(title  ) )
+}
+
+object Post {
+  val dateParser    = new SimpleDateFormat("yyyy-MM-dd"    )
+  val dateFormatter = new SimpleDateFormat("MMMMM dd, yyyy")
+  
+  def fromFile(f: File): Post = {
+    val postName = """(\d{4}-\d{2}-\d{2})-(.*)\.md""".r
+    f.getName match { case postName(dateStr, title) => Post(
+      inFile = f
+    , date   = dateParser.parse(dateStr)
+    , title  = title) }
   }
-
-  // def index(vars: Map[String, String]): Ef[Unit] =
-  //   for {
-
-  //   }
 }
