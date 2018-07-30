f753e1e5169e34d43c606d6d2a676e473acba216
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2018-07-31 01:52:09 +0300

    Customisable thera script

--- thera.sh
+++ thera.sh
@@ -1,10 +1,30 @@
+#!/usr/bin/env bash
+
 HELP="Usage: thera <command>, where <command> is one of:
   start - Start Thera
   stop  - Stop Thera
   bash  - open bash console at Thera's Docker image"
+
 SELF_DIR=`pwd`
+COMMAND_OVERRIDES="$SELF_DIR/thera.sh"
+IMAGE_OVERRIDE="$SELF_DIR/Dockerfile"
+DEFAULT_IMAGE_NAME=thera:latest
 
 function start_thera {
+  if [ -a $IMAGE_OVERRIDE ]; then
+    if ! [ -z ${IMAGE_NAME+x} ]; then  # https://stackoverflow.com/a/13864829
+      echo "Dockerfile override active. Building image with name $IMAGE_NAME"
+      docker build -t $IMAGE_NAME .
+    else
+      echo "Dockerfile detected, but IMAGE_NAME variable is not set.
+Using standard image, thera:latest. Please set IMAGE_NAME environment variable in thera.sh file
+to name your image and use it instead of the standard one."
+      IMAGE_NAME=$DEFAULT_IMAGE_NAME
+    fi
+  else
+    IMAGE_NAME=$DEFAULT_IMAGE_NAME
+  fi
+
   docker run -td \
     -v $SELF_DIR/_volumes/home:/root \
     -v $SELF_DIR:/root/thera \
@@ -12,7 +32,7 @@ function start_thera {
     -p 8888:8888 \
     --name thera \
     --rm \
-    thera:latest
+    $IMAGE_NAME
 }
 
 function stop_thera {
@@ -22,6 +42,11 @@ function stop_thera {
 function run_on_thera {
   docker exec -ti thera $@
 }
+
+if [ -a $COMMAND_OVERRIDES ]; then
+  echo "Loading functions from $COMMAND_OVERRIDES"
+  . $COMMAND_OVERRIDES
+fi
 
 case $1 in
     start) start_thera;;
