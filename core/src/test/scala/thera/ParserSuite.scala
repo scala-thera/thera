cc233826667abadb5f1ef204a36942100fe2f6fb
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2020-05-15 20:21:39 +0200

    Cleaner named tests for parser

--- core/src/test/scala/thera/ParserSuite.scala
+++ core/src/test/scala/thera/ParserSuite.scala
@@ -9,12 +9,16 @@ class ParserSuite extends TestSuite with ParserSuiteHelpers {
   val tests = Tests {
     test("Parser") {
 
-      test("Named tests") - toParse.foreach { name =>
-        val source = scala.io.Source.fromURL(classOf[ParserSuite].getResource(s"/example/$name.html")).mkString
-
-        fastparse.parse(source, module(_)) match {
-          case Success(result, _) => assert(result.toString == fileResult(name))
-          case f: Failure => throw new RuntimeException(f.toString)
+      test("Named tests") - toParse.foreach { case (name, input, output) =>
+        fastparse.parse(input, module(_)) match {
+          case Success(result, _) =>
+            try assert(result.toString == fileResult(name))
+            catch {
+              case t: AssertError =>
+                sys.err.println(s"Named test failure: $name")
+                throw t
+            }
+          case f: Failure => throw new RuntimeException(s"Named test failure: $name\n${f.toString}")
         }
       }
 
@@ -81,80 +85,23 @@ trait ParserSuiteHelpers {
   def p[A](str: String, p: P[_] => P[A]): String =
     parse(str, p(_)).get.value.toString
 
-  val toParse = List("index", "fun_frag", "html-template", "three-frag")
+  def toParse: List[(String, String, String)] = {
+    val classLoader = classOf[ParserSuite].getClassLoader
 
-  val fileResult: Map[String, String] = Map(
-"index" -> """
-Function(List(),{
-  "title" : "This stuff works!",
-  "one" : "1",
-  "two" : "2",
-  "three" : {
-    "four" : "4"
+    def listFilesInDir(dir: String): List[File] = {
+      val url = loader.getResource(dir)
+      val path = url.getPath
+      new File(path).listFiles.asScala
+    }
+
+    val cases: List[File] = listFilesInDir("/parser/input")
+
+    cases.map { inputFile =>
+      val name = f.getName
+      val outputFile = new File(inputFile.parentDirectory, "/output/$name")
+      val input = Source.fromFile(inputFile).mkString
+      val output = Source.fromFile(outputFile).mkString
+      (name, input, output)
+    }
   }
-},Leafs(List(Text(I have numbers ), Variable(List(one)), Text(, ), Variable(List(two)), Text( and ), Variable(List(three, four)), Text(. If I add them, here is what I get: ), Variable(List(three-frag)), Text(. I can also do ), Call(List(fun_frag),List(Text(simple), Text(nice))), Text( and ), Call(List(fun_frag),List(Text(complex ,
-args), Text(awesome))), Text( calls. I hope to make $1,000,000 on this stuff I can also call ), Call(List(fun_frag),List(Call(List(fun_frag),List(Text($1,000,000), Text(good))), Text(recursive))), Text(. We can also escape with "\".
-))))""".tail,
-
-"fun_frag" -> """
-Function(List(msg, msg2),{
-
-},Leafs(List(Text(The very ), Variable(List(msg2)), Text( ), Variable(List(msg)), Text(
-))))""".tail,
-
-"html-template" -> """
-Function(List(body),{
-
-},Leafs(List(Text(<!DOCTYPE html>
-<html>
-<head>
-  <title>), Variable(List(title)), Text(</title>
-</head>
-<body>
-), Variable(List(body)), Text(
-<div>
-  ), Call(List(map),List(Function(List(dummy),{
-
-},Text(buf)))), Text(
-
-  <h1>Our users</h1>
-
-  ), Call(List(map),List(Function(List(a, c),{
-
-},Variable(List(a))))), Text(
-  ), Call(List(map),List(Leafs(List(Text(b ), Variable(List(a)))))), Text(
-
-  ), Call(List(map),List(Variable(List(our_users)), Function(List(u),{
-
-},Leafs(List(Text(
-    <p>
-      Name : ), Variable(List(u, name)), Text(
-      Email: ), Variable(List(u, email)), Text(
-    </p>
-
-    Warnings:
-    <ul>
-      ), Call(List(map),List(Variable(List(u, warnings)), Function(List(w),{
-
-},Leafs(List(Text(        <li>), Variable(List(w)), Text(</li>
-      )))))), Text(
-
-    </ul>
-
-    Btw here's how a smiley mustache is drawn: }
-
-    And here's some Scala code of that user:
-
-    ```{.scala file="foo.scala"}
-    ```
-  )))))), Text(
-</div>
-</body>
-</html>))))""".tail,
-
-"three-frag" -> """
-Function(List(),{
-
-},Text(I've got three as a result!))""".tail
-  )
 }
