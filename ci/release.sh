3773a09fb22926f83325367125bdf4f695b691bf
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2020-05-20 14:15:06 +0200

    Disable tty when publishing

--- ci/release.sh
+++ ci/release.sh
@@ -5,7 +5,7 @@ echo $PGP_SECRET | base64 --decode | gpg --import --no-tty --batch --yes
 
 ./mill thera.publish \
   --sonatypeCreds $SONATYPE_USER:$SONATYPE_PW \
-  --gpgArgs --passphrase=$PGP_PASSPHRASE,--batch,--yes,-a,-b \
+  --gpgArgs --passphrase=$PGP_PASSPHRASE,--batch,--yes,-a,-b,--no-tty \
   --readTimeout 600000 \
   --release true \
   --signed true
