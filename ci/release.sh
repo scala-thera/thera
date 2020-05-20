22d4d9662750400fb79a99432fd87d82f0cce47a
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2020-05-20 14:10:32 +0200

    Typo fix

--- ci/release.sh
+++ ci/release.sh
@@ -4,7 +4,7 @@ set -eux
 echo $PGP_SECRET | base64 --decode | gpg --import --no-tty --batch --yes
 
 ./mill thera.publish \
-  --sonatypeCreds $SONATYPE_USER:$SONATYPE_PASSWORD \
+  --sonatypeCreds $SONATYPE_USER:$SONATYPE_PW \
   --gpgArgs --passphrase=$PGP_PASSPHRASE,--batch,--yes,-a,-b \
   --readTimeout 600000 \
   --release true \
