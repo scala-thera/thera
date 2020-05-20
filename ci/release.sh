6f4665503f99ae55189f5466c1736d54dd47cbf0
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2020-05-20 14:22:55 +0200

    Set proper tty

--- ci/release.sh
+++ ci/release.sh
@@ -2,10 +2,11 @@
 set -eux
 
 echo $PGP_SECRET | base64 --decode | gpg --import --no-tty --batch --yes
+export GPG_TTY=$(tty)
 
 ./mill thera.publish \
   --sonatypeCreds $SONATYPE_USER:$SONATYPE_PW \
-  --gpgArgs --passphrase=$PGP_PASSPHRASE,--batch,--yes,-a,-b,--no-tty \
+  --gpgArgs --passphrase=$PGP_PASSPHRASE,--batch,--yes,-a,-b \
   --readTimeout 600000 \
   --release true \
   --signed true
